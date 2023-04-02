de4f49c48f4c3f184cad8d2094e05aa9504b5bda
Charles Nicholson <charles.nicholson@gmail.com>
2023-04-02 10:56:58 -0400

    windows: build in subdir, ignore vscode stuff (#64)

--- make-win.bat
+++ make-win.bat
@@ -1,4 +1,5 @@
 @echo off
-cl.exe /nologo /std:c17 /W4 /WX /Osy /EHsc /GL /c improg.c || exit /b 1
-cl.exe /nologo /std:c17 /W4 /WX /Osy /EHsc /GL /c main.c || exit /b 1
-link.exe /NOLOGO /LTCG /WX /out:demo.exe improg.obj main.obj || exit /b 1
+if not exist "%~dp0build" md "%~dp0build"
+cl.exe /nologo /std:c17 /W4 /WX /Osy /EHsc /GL /c "%~dp0improg.c"  /Fo"%~dp0build\improg.obj" || exit /b 1
+cl.exe /nologo /std:c17 /W4 /WX /Osy /EHsc /GL /c "%~dp0main.c" /Fo"%~dp0build\main.obj" || exit /b 1
+link.exe /NOLOGO /LTCG /WX /out:"%~dp0build\demo.exe" "%~dp0build\improg.obj" "%~dp0build\main.obj" || exit /b 1
