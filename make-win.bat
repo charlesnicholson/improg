bcbca3816f111cfc3f5e4146071ce7376523fc2f
Charles Nicholson <charles.nicholson@gmail.com>
2023-04-10 09:36:25 -0400

    move to cmake (#75)
    * move to cmake
    
    * pr fixes
    
    * c17
    
    * libm
    
    * shut up clang
    
    * get ninja, better cmake invocation
    
    * fix up path
    
    * debug
    
    * curl follows redirects
    
    * shove ninja under build
    
    * ls
    
    * Realpath?
    
    * python path?
    
    * fix quotes and parens
    
    * don't need to escape subshell quotes?
    
    * delete comments, normalize invocation

--- make-win.bat
+++ make-win.bat
@@ -1,5 +1,5 @@
 @echo off
 if not exist "%~dp0build" md "%~dp0build"
 cl.exe /nologo /Z7 /std:c17 /W4 /WX /Osy /EHsc /GL /c "%~dp0improg.c" /I include /Fo"%~dp0build\improg.obj" || exit /b 1
-cl.exe /nologo /Z7 /std:c17 /W4 /WX /Osy /EHsc /GL /c "%~dp0main.c" /I include /Fo"%~dp0build\main.obj" || exit /b 1
-link.exe /NOLOGO /DEBUG:FULL /LTCG /WX /out:"%~dp0build\demo.exe" "%~dp0build\improg.obj" "%~dp0build\main.obj" || exit /b 1
+cl.exe /nologo /Z7 /std:c17 /W4 /WX /Osy /EHsc /GL /c "%~dp0examples\improg-demo.c" /I include /Fo"%~dp0build\improg-demo.obj" || exit /b 1
+link.exe /NOLOGO /DEBUG:FULL /LTCG /WX /out:"%~dp0build\demo.exe" "%~dp0build\improg.obj" "%~dp0build\improg-demo.obj" || exit /b 1
